928b16b0a1e96a3697a4d51e24eeae1a8ad4b6df
skgt5454 <sakshamg086@gmail.com>
2026-07-12 13:52:52 +0000

    conversionofdatatypes

---
+++ .devcontainer/01_basics/conversionofdatatypes.js
@@ -0,0 +1,21 @@
+let score = "66"
+// js me hme pta h ye valuenumber h but jb frontened me user dega to hme nhi pta hoga ki value kis type ki h to hme console wale method se check krna hoga
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber)) //it is of type of number
+console.log(valueInNumber) // it is of type of NaN
+// NaN is a special type that is also checkable
+// if we put score  = null so type of valueinNumber is number and theconsole.log(valueinNumber) is 0
+// if score = true then console...typeof is number and console..valueInNumber is 1 
+// if score = "saksham" then console...typeof is number and console..valueInNumber is NaN
+let score2 = 3312
+console.log(typeof score)
+console.log(typeof score2)
+// let isloggedIn = 1
+// let booleanisloggedIn = Boolean(isloggedIn)
+// console.log(booleanisloggedIn) in this case answer is true
+// logggedin me " " ye dalue to aaega false
+// loggedin me "hitesh" ye daalu to aaega true
+// if score = 33 and convert it in to string so ans is 33 but type is string
