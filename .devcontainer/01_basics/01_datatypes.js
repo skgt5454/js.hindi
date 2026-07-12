6904d9a4d2e5801d78a797b812a69947947120de
skgt5454 <sakshamg086@gmail.com>
2026-07-12 13:08:33 +0000

    datatypes and variables

---
+++ .devcontainer/01_basics/01_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";//treat all js code as newer version
+//alert("hello") to ye error dega because we use the node.js not browser as we know ki javascript ka engine browser me chipa hua h to use bahr chalane ke liye ek  node.js naam ka runtime environment bna diya h programming krne ke liye
+//semicolon likhna koi zruri nhi h
+//documentation hum tc39 pr aur mdn pr dekhenge
+
+let name = "saksham"
+let age = 3
+let isloggedIn =false
+
+//#datatypes
+//#primitive datatypes
+// number has range 2 to th power 53
+// bigint it use for store very large number
+// string = " "
+// boolean
+// null -> standalone value
+// undefined ->means koi value define nhi h
+// symbol => used for uniqueness
+
+// object aage pdhenge
+
+console.log(typeof null)
+console.log(typeof undefined)
+
+// so hmne dekh ki null ek object hai so ise aage pdhenge 
---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 14543
+let accountEmail = "skhydg@gmail.com"
+var accountno = "7851"
+// accountEmail = "hh@gmail.com"
+accountno = "84555"
+//prefer not to use var in js due to the problem of block scope and functional scope
+console.table([accountId,accountno,accountEmail])
+// ise sara data tabular form me aa jaega 
