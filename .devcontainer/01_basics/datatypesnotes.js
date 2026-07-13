9f14fde1166bc7384c13813500061af6a60a748c
skgt5454 <sakshamg086@gmail.com>
2026-07-13 07:21:11 +0000

    notes datatypes1

---
+++ .devcontainer/01_basics/datatypesnotes.js
@@ -0,0 +1,25 @@
+// DATATPES are classified on basis-
+// memory me data kis tarah se store hote hai aur 
+// kis tarah se aap data ko excess kr skte hai
+//1.primitive - call by value  2.non-primitive/reference -  call by reference
+// primitive datatypes ko jb bhi hm khi se khi copy krte h to (data ki ek copy) hi copy hoti h na ki orginal value.. jo bhi changes hm krte h vo copy me hote h only
+// 7 types - String,Number,Boolean ,null,undefined,Symbol(kis bhi value ko unique bnane me),BigInt
+
+// js is dynamically typed language because here we are not need to declare the datatype for a value
+// example is const name = "saksham"  here datatype is decided at runtime
+// but in static typed me datatype  define krna pdta h aur isme datatype is decided at compile time
+// like int age = 25
+
+
+// 100,100.25 b0th are in number category
+// false, true both are in boolean
+// null in the null category
+//let emailid;  ye undefined h
+// const id  = Symbol("123")
+// const anid  = Symbol("123")
+// console.log(id == anid)   so returns false
+// const bigNumber = 1555588866n
+
+
+//non-primitive(reference) 
+ // Array,objects,functions
