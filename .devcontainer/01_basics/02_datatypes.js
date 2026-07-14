b50fb0105bf7e9bcef6726b7e5bbf9815fc39278
skgt5454 <sakshamg086@gmail.com>
2026-07-14 14:28:43 +0000

    datatypes2

--- .devcontainer/01_basics/02_datatypes.js
+++ .devcontainer/01_basics/02_datatypes.js
@@ -1,5 +1,6 @@
-//arrays -const heroes = ["shaktiman","naagraj"]
+//arrays -  const heroes = ["shaktiman","naagraj"]
 //  console.log(typeof heroes)  - object 
+
 //objects - let myobj = {name : "sak",age:22}
 // so idhar jo bhi myobj me likha h wo object hai
 // console.log(typeof myobj) - object
@@ -11,3 +12,21 @@
 
 // const bigInt = 58446151864168416n
 // console.log(typeof bigInt) -->> bigint
+
+/// memory has two types = stack and heap
+// in primitive stack is used and in non primitive heap is used
+
+
+// let myyt = "saksham"
+// let anmyyt = myyt
+// anmyyt = "raja"
+// console.log(myyt) - saksham
+// console.log(anmyyt) - raja
+
+// in primitive datatypes there is call by value so original value of myyt not change 
+
+let userone = {
+    name :"saksh",
+    age : 15
+}
+console.log(userone.name) // ye method h . lgake kisi bhi cheez ka name de do uska value pta lgane ke liye
