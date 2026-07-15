403d2a1b5bb7765b494c8dfe4f6c2eaf1a569bac
skgt5454 <sakshamg086@gmail.com>
2026-07-15 17:54:39 +0000

    math.num

---
+++ .devcontainer/01_basics/number_maths.js
@@ -0,0 +1,19 @@
+/////++++++++++ MATHS++++++++////////
+// math is an object in js 
+// console.log(Math) ---> object 
+//console.log(Math.abs(-4)) --> this method change neg to pos
+// console.log(Math.round(4.6)) -- 7
+//console.log(Math.ceil(4.2)) -- ceiling method return always greater value -->5
+//console.log(Math.floor(4.2))--- floor method return always lesser value-->4
+// min,max method bhi h
+// it has many constants like pi ,E and functions pow(),sqrt()
+// console.log(Math.random()) -- ye method always different value print krega 0 to 1 ke beech me 
+
+// in case of any problem like we hme koi problem me ya game bnana h to no. lene h 1 se 10 ke beech me to hme
+// console.log((Math.random*10)+1)
+// console.log(Math.floor((Math.random()*10)+1))--> return integer
+
+// const min = 10
+// const max = 20
+console.log(Math.floor(Math.random()*(max-min+1))+min) // agr mujhe chahiye 10 se 20 ke beech me ho value
+// formula --> 
