db5ce1cbdecf802658144d8ae73cde874b8cfbc0
skgt5454 <sakshamg086@gmail.com>
2026-07-19 19:14:53 +0000

    01_ifelse

---
+++ .devcontainer/04_controlflow/01_if-else.js
@@ -0,0 +1,45 @@
+// control flow means age hme jaise ki particular condition pr particular code run krna chahte h to hm if vagera use krte h to us process ko control flow khte h
+// jo var h uska jo scope h vo h completely global.. so ye function ke andar aur bahr dono jagah.. to console.log() execute ho jaaega
+// ex
+// const score = 200
+// if(score==200)
+//   {let power = "fly"
+//      console.log(power) // fly
+// }
+// console.log(power) // error
+// but if var hota let ki jagah to bahr wala bhi execute ho jaata
+// so hum let hi use krte h
+
+
+// const balance = 1000
+// if(balance>100) console.log(balance) // 100
+
+// const b = 500
+// if(b >=200) console.log(100),console.log(855); // idhar semicolon lgate h but this code is not recommendable
+
+
+// const userloggedIn = true
+// const debitcard = true
+// const LoggedInFromGoogle = false
+// const loggedInFromEmail = true
+
+// if(userloggedIn && debitcard && 2==3) // so  hum multiple operator lga skte h
+// {
+//     console.log("buy all the course")
+// }
+// if(LoggedInFromGoogle || loggedInFromEmail || true)
+// {
+//     console.log("user logged in")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
