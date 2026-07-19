411b76f87b4ea4417d4d02ab317d56a10151f1ca
skgt5454 <sakshamg086@gmail.com>
2026-07-19 19:15:15 +0000

    switch

---
+++ .devcontainer/04_controlflow/02_switch.js
@@ -0,0 +1,35 @@
+const month =5
+
+switch(month)
+{
+    case 1:
+        console.log("january")
+        break;
+    case 2:
+        console.log("feb")
+        break;
+    case 3:
+        console.log("march")
+        break;
+    case 4:
+            console.log("april")
+            break;
+    case 5:
+            console.log("may")
+            break;
+    case 6:
+            console.log("june")
+            break;
+    case 7:
+            console.log("july")
+            break;
+    default:
+            console.log("no month")
+            break;
+}
+//jaha match ho gya wo to print hoga aur isme break nhi lga to agla bhi execute hoga then agr agle pr bhi break nhi h to vo bhi execute hoga
+
+
+
+
+
