17735058d05bf98971eca1819fb186aa237b0208
skgt5454 <sakshamg086@gmail.com>
2026-07-22 09:26:38 +0000

    loop-2

--- .devcontainer/04_controlflow/loop2.js
+++ .devcontainer/04_controlflow/loop2.js
@@ -1,133 +1,130 @@
+// loops -- ab jo bhi loop aayenge vo array and objects specific h
+//for...of works on iterables
+
+// for...of works on iterables
+// An iterable is an object that has a special method called Symbol.iterator
+// ["", "", ""]
+// [{}, {}, {}]
+
+// const arr = [1, 2, 3, 4, 5]
+
+// for (const num of arr) {
+//     console.log(num);
+// } // here in for of loop num likhte hi value print ho gyi
+
+// const greetings = "Hello world!"
+// for (const greet of greetings) {
+//     //console.log(`Each char is ${greet}`)
+// } // sare characters print ho jayenge
+
+// Map - it is an object that hold key value pairs and print in original insertion order and duplication of key and values pairs are not occur 
+
 // const map = new Map()
-// map.set("in" , "india")
-// map.set("usa" , "united states of america")
-// map.set("in" , "indo") 
-// for(const key in map) {
-//    console.log(key)
-// } // ye kuch print nhi krega for-in loops map pr kaam nhi krega
-
-// for each loop
-// const coding = ["js", "ruby","java","c","python"]// kya hota h kuch loop arrays ke andar unki properties me add kr diye jaate h
-// coding.forEach( function(item){console.log(item)
-// })
-// answer
-// ruby
-// java
-// c
-// python
-//coding.forEach((item)=>{console.log(item)}) //--> for arrow functions same result as upper
-
-
-
-// function result(item)
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+//map.set('IN', "Ina") // if i write this then value of IN is replace by india
+//  console.log(map);
+// for(const key of map)
 // {
-//    console.log(item)
+//     console.log(key)
+// } //answer --> [ 'IN', 'India' ]
+// [ 'USA', 'United States of America' ]
+// [ 'Fr', 'France' ]
+
+// for (const [key, value] of map) {
+//      console.log(key, ":-" , value);
 // }
-// coding.forEach(result)
 
-// const coding = ["js", "ruby","java","c","python"]
-// coding.forEach( function(item,index,coding)
-// {console.log(item,index,coding)})
-// js 0 [ 'js', 'ruby', 'java', 'c', 'python' ]
-// ruby 1 [ 'js', 'ruby', 'java', 'c', 'python' ]
-// java 2 [ 'js', 'ruby', 'java', 'c', 'python' ]
-// c 3 [ 'js', 'ruby', 'java', 'c', 'python' ]
-// python 4 [ 'js', 'ruby', 'java', 'c', 'python' ]
+//for of loop on object 
+// const myObject = { // -> here myobject is not iteratable
+//     game1: 'NFS',
+//     game2: 'Spiderman'
+// }
 
-// this is very used in projects
-// const mycode = [
-//    {
-//       lname:"js",
-//       lfile:"javascript"
-//    },
-//    {
-//       lname:"c",
-//       lfile:"cdhdh"
-//    },
-//    {
-//       lname:"j",
-//       lfile:"java"
-//    }
-//    ]
-//    mycode.forEach( (item)=>{console.log(item.lname );} )
-// answers 
-// js
-// c
-// j
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+// } // error
+
+// for in loop
+// const myObject = {
+//     js: 'javascript',
+//     cpp: 'C++',
+//     rb: "ruby",
+//     swift: "swift by apple"
+// }
+// for (const key in myObject) {
+//     //console.log(key);
+// } // it return keys
+
+// for (const key in myObject) {
+//     //console.log(myObject[key]);
+// } // it return objects
+
+// for (const key in myObject) {
+//     //console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+ // for in loop for arrays
+//const programming = ["js", "rb", "py", "java", "cpp"]
+//in for-in loops in array print only keys if we right num
+// for (const num in programming) {
+//     //console.log(num)
+//     console.log(programming[num]);
+// }
 
 
+// map is not iteratable in case of for in loop. doesnt work on this
+//for...in works on object properties (keys)
+//A Map does not store its data as normal object properties
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
 
-//In the for each loop hm return nhi kr skte
-// const nums = [1,2,3,4,5,6,7,8]
-// const mynums = nums.filter( (num) => num>5) // isme kya hota h jo bhi number ye condition ko fulfill krta h wo return ho jata h
-// console.log(mynums)
+// for (const key in map) {
+//     console.log(key);
+//} // ab loop nhi chlega 
 
-//const mynums = nums.filter( (num) => {return num>4})  // in curly braces return likhna pdta h kyuki ek scope chalu ho jaat h curly braces se
-//console.log(mynums)
 
-// const nums = [1,2,3,4,5,6,7,8]
-// const newnums = []
-// nums.forEach((num) =>{
-//     if(num>5)
+// for each loop --ye loop ka parameter h to ise bydefault inject kr rkha h arrays me
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+//coding.forEach(callbackfunction) --> callback function is just a function given to do work on that
+// coding.forEach( function (val){
+//     console.log(val);
+// } )
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// function printMe(item){
+//     console.log(item);
+// }
+ // coding.forEach(printMe)
+
+// // coding.forEach( (item, index, arr)=> {
+// //     console.log(item, index, arr);
+// // } )
+
+// const myCoding = [
 //     {
-//         newnums.push(num)
-//     }
-// })
-//  console.log(newnums)
-
-// filter method -- > ye kisi condition ke hisab se particular subarray ko print krata h
-const books = [
-    {
-        name : "book1" , genre : "fiction"
-    },
-    {
-        name : "book2" , genre : "non-fiction"
-    },
-    {
-        name : "book3" , genre : "fiction"
-    },
-    {
-        name : "book4" , genre : "fiction"
-    },
-    {
-        name : "book1" , genre : "non fiction"
-    },
-    {
-        name : "book6" , genre : "adventure"
-    }
-];
-  
-let nums = books.filter((book) => book.genre === "fiction")
-console.log(nums)
-// so jb hum dusri baar filter method to isme nums me change nhi hua blki ek new nums print ho gya
-nums = books.filter((book) =>book.genre =="adventure")
-console.log(nums)
-
-// const users = books.filter((user)=>{
-//     return user.name>"book1"
-// })
-// console.log(users)
-// [
-//   { name: 'book2', genre: 'non-fiction' },
-//   { name: 'book3', genre: 'fiction' },
-//   { name: 'book4', genre: 'fiction' },
-//   { name: 'book6', genre: 'adventure' }
+//         languageName: "javascript",
+//         languageFileName: "js"
+//     },
+//     {
+//         languageName: "java",
+//         languageFileName: "java"
+//     },
+//     {
+//         languageName: "python",
+//         languageFileName: "py"
+//     },
 // ]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// myCoding.forEach( (item) => {
+    
+//     console.log(item.languageName);
+// } )
 
