df12262ac5ee1c6ecb094dc64673cab16939b672
skgt5454 <sakshamg086@gmail.com>
2026-07-22 06:40:34 +0000

    loop-1

--- .devcontainer/04_controlflow/loop.js
+++ .devcontainer/04_controlflow/loop.js
@@ -1,34 +1,56 @@
-// for loops
-// jaise hi for lgaya to uska mtlb for keyword initialise hua
-for(let i = 0;i<10;i++)
-{
-    const element = i;
-    if(element==5)
-    {
-        console.log("5 is not defined")
-    }
-}
-//console.log(element)
-//idhar ab hum i aur element print karwaenge to print nhi hoga kyuki vo scope ke andar h
-//agr mujhe kisi bhi word ko sab jagah ek sath change krna h to use select kro then jitni jagah change krna h utni baar ctrl+d daba do
-for(let i = 0;i<=10;i++) // idhar hum i use kr skte h h kyuki uska ilaka/ scope vhi tk ka tha
-{
-    console.log(`outer loop value:${i}`)
-    for(let j = 0;j<=10;j++) // idhar hum i use kr skte h h kyuki uska ilaka/ scope vhi tk ka tha
-{
-    console.log(`inner loop value: ${j}`)
-}
-}
+// // for loops
+// // jaise hi for lgaya to uska mtlb for keyword initialise hua
+// for(let i = 0;i<10;i++)
+// {
+//     const element = i;
+//     if(element==5)
+//     {
+//         console.log("5 is not defined")
+//     }
+// }
+// //console.log(element)
+// //idhar ab hum i aur element print karwaenge to print nhi hoga kyuki vo scope ke andar h
+// //agr mujhe kisi bhi word ko sab jagah ek sath change krna h to use select kro then jitni jagah change krna h utni baar ctrl+d daba do
+// for(let i = 0;i<=10;i++) // idhar hum i use kr skte h h kyuki uska ilaka/ scope vhi tk ka tha
+// {
+//     console.log(`outer loop value:${i}`)
+//     for(let j = 0;j<=10;j++) // idhar hum i use kr skte h h kyuki uska ilaka/ scope vhi tk ka tha
+// {
+//     console.log(`inner loop value: ${j}`)
+// }
+// }
 
-// break and continue statement
-for(let i = 0;i<5;i++)
-{
-    if(i==5)
-    {
-        break;
-    }
-    console.log(`value of i is ${i}`)
-}
+// // break and continue statement
+// for(let i = 0;i<5;i++)
+// {
+//     if(i==5)
+//     {
+//         break;
+//     }
+//     console.log(`value of i is ${i}`)
+// }
+
+// while and do while loop
+//let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+// let myArray = ['flash', "batman", "superman"]
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     //console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+// let score = 11
+
+// do {
+//     console.log(`Score is ${score}`);
+//     score++
+// } while (score <= 10);
 
 
 
